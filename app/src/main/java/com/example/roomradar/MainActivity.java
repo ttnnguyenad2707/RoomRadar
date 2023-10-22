586ac001579921925e507f815aaaa83762a3741a
QuangHuy <nguyenvanquanghuy051102@gmail.com>
2023-10-23 01:58:32 +0700

    add login page

--- app/src/main/java/com/example/roomradar/MainActivity.java
+++ app/src/main/java/com/example/roomradar/MainActivity.java
@@ -2,7 +2,11 @@ package com.example.roomradar;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.widget.TextView;
+
+import java.io.Serializable;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -10,5 +14,11 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Intent intent = getIntent();
+        User user = (User) intent.getSerializableExtra("user");
+
+        TextView lastname = findViewById(R.id.Textviewlastname);
+        lastname.setText(user.getLastname());
     }
 }
