89284d7d2e68b4f55554a2a741e9a15ff2f977b6
Nguyen Tran <trantrungnguyenad@gmail.com>
2023-11-06 23:00:30 +0700

    layout posted of user

--- app/src/main/java/com/example/roomradar/ChatFragment.java
+++ app/src/main/java/com/example/roomradar/ChatFragment.java
@@ -1,5 +1,6 @@
 package com.example.roomradar;
 
+import android.content.Intent;
 import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
@@ -7,6 +8,7 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -59,6 +61,14 @@ public class ChatFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_chat, container, false);
+        View view = inflater.inflate(R.layout.fragment_chat, container, false);
+        view.findViewById(R.id.profile_listPost).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getActivity(),ListPostOfUser.class);
+                startActivity(intent);
+            }
+        });
+        return view;
     }
 }
