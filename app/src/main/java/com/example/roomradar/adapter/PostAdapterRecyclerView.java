236e6f8ac16e04efd6faf91e293dbe5ad14e411c
nhat <nhatyhnl1258@gmail.com>
2023-11-07 21:24:22 +0700

    search

--- app/src/main/java/com/example/roomradar/adapter/PostAdapterRecyclerView.java
+++ app/src/main/java/com/example/roomradar/adapter/PostAdapterRecyclerView.java
@@ -10,7 +10,6 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-import com.bumptech.glide.Glide;
 import com.example.roomradar.R;
 import com.example.roomradar.model.Post;
 
