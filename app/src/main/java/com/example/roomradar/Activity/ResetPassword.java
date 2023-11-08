2e650a3d7d6978c1d0647267251309cc14933db1
QuangHuy <nguyenvanquanghuy051102@gmail.com>
2023-11-09 06:03:14 +0700

    update1

---
+++ app/src/main/java/com/example/roomradar/Activity/ResetPassword.java
@@ -0,0 +1,16 @@
+package com.example.roomradar.Activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.roomradar.R;
+
+public class ResetPassword extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_reset_password);
+    }
+}
